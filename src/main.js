eb6cb9d8baba54ee07f1cf30aa58cde7ba5920ce
Kyour.cn <kyour@outlook.com>
2023-10-29 21:44:40 +0800

    feat: 新增cell组件和pinia

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import {
 import App from "./App.vue";
 
 import "petal-ui/style/theme.css"
+import * as Pinia from 'pinia';
 
 export function createApp() {
 	const app = createSSRApp(App);
+	app.use(Pinia.createPinia());
 	return {
 		app,
+		Pinia
 	};
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,13 +3,45 @@
         <PuButton size="mini">开始体验</PuButton>
         <PuButton size="small">开始体验</PuButton>
         <PuButton :block="true" size="normal">开始体验</PuButton>
-        <PuButton :block="true" size="large">开始体验</PuButton>
+        <PuButton :block="true" size="large" @click="setTheme">切换主题</PuButton>
+
+    </view>
+
+    <view>
+
+        <PuCellGroup>
+            <PuCell title="设备名称" value="小P的手机" />
+        </PuCellGroup>
+
+        <PuCellGroup>
+            <PuCell title="型号名称" value="HUAWEI P60 Pro" />
+            <PuCell title="型号代码" value="ABR-AL90" />
+            <PuCell title="HarmonyOS版本" value="4.0.0" />
+        </PuCellGroup>
     </view>
 </template>
 
 <script setup>
 
 import PuButton from "petal-ui/components/button"
+import PuCell from "petal-ui/components/cell"
+import  PuCellGroup from "petal-ui/components/cell-group"
+
+import {usePetalUiStore} from "petal-ui/stores/petal-ui";
+import {storeToRefs} from "pinia";
+
+const puiStore = usePetalUiStore()
+
+const { isDark } = storeToRefs(puiStore)
+
+console.log(isDark)
+
+const setTheme = () => {
+    // document.documentElement.style.setProperty('--pui-color-primary', '#a223c5');
+
+    puiStore.isDark =  !puiStore.isDark
+
+}
 
 </script>
 
@@ -20,5 +52,8 @@ import PuButton from "petal-ui/components/button"
 }
 .content * {
     margin-bottom: 10rpx;
+}
+page {
+    background-color: #F3F4F6;
 }
 </style>
