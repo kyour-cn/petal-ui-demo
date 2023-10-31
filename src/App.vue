29029cf164c27f1bb7c31ff8c89952b9b24c230d
Kyour.cn <kyour@outlook.com>
2023-10-31 17:33:29 +0800

    feat: 完善主题暗黑模式背景适配

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,36 @@
 import {watch} from "vue";
 import {usePetalUiStore} from "petal-ui/stores/petal-ui";
 
+const setPageBg = (color) => {
+
+    //#ifdef MP-WEIXIN
+    uni.setBackgroundColor({
+        backgroundColor: color,
+    });
+    //#endif
+
+    //#ifdef H5
+    // 获取页面元素
+    const page = document.querySelector('uni-app');
+    // 设置背景色
+    page.style.backgroundColor = color;
+    //#endif
+}
+
 export default {
     onLaunch: function () {
 
         const puiStore = usePetalUiStore()
 
         // 监听isDark变化
-        watch(() => puiStore.isDark, (newVal) => {
-
-            //#ifdef MP-WEIXIN
-            uni.setBackgroundColor({
-                backgroundColor: 'var(--pui-bg-page)' + (newVal ? '-dark' : ''),
-            });
-            //#endif
-
-            //#ifdef H5
-            // 获取页面元素
-            const page = document.querySelector('uni-app');
-            // 设置背景色
-            page.style.backgroundColor = 'var(--pui-bg-page' + (newVal ? '-dark' : '') + ')';
-            //#endif
-
+        watch(() => puiStore.dark, (newVal) => {
+            setPageBg(puiStore.theme['bg-page'])
         })
+
+        setTimeout(() => {
+            setPageBg(puiStore.theme['bg-page'])
+        }, 10)
+
     },
     onShow: function () {
 
@@ -34,11 +42,3 @@ export default {
     }
 }
 </script>
-
-<style>
-/*每个页面公共css */
-uni-app {
-    background: var(--pui-bg-page);
-    height: 100%;
-}
-</style>
