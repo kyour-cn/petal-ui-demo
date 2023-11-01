c4da883cf493902dead10812026b84564b486c50
Kyour.cn <kyour@outlook.com>
2023-11-01 21:16:41 +0800

    feat: 新增更多组件演示

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,53 @@
 <template>
     <App>
+        <Navbar title="PetalUI" />
+        
         <view class="content">
-            <PuButton size="mini" background="green" text="开始体验" />
-            <PuButton size="small" disabled background="error">开始体验</PuButton>
-            <PuButton :block="true" size="normal">开始体验</PuButton>
-            <PuButton :block="true" size="large" @click="setTheme" background="#5555FF">切换主题</PuButton>
+            <PuiButton size="mini" background="green" text="开始体验" />
+            <PuiButton size="small" disabled background="error">开始体验</PuiButton>
+            <PuiButton :block="true" size="normal">开始体验</PuiButton>
+            <PuiButton :block="true" size="large" @click="setTheme" background="#5555FF">切换主题</PuiButton>
         </view>
 
         <view>
-            <PuCellGroup>
-                <PuCell title="设备名称" value="小P的手机"/>
-            </PuCellGroup>
+            <CellGroup>
+                <Cell title="版本信息" value="v0.01"/>
+            </CellGroup>
 
-            <PuCellGroup>
-                <PuCell title="型号名称" value="HUAWEI P60 Pro"/>
-                <PuCell title="型号代码" value="ABR-AL90"/>
-                <PuCell title="HarmonyOS版本" value="4.0.0"/>
-            </PuCellGroup>
+            <CellGroup>
+                <Cell title="名称" value="PetalUI"/>
+                <Cell title="框架" value="Vue3"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+            </CellGroup>
         </view>
+        <pui-tabbar>
+            <TabbarItem />
+            <TabbarItem />
+            <TabbarItem />
+        </pui-tabbar>
     </App>
 
 </template>
@@ -25,9 +55,12 @@
 <script setup>
 
 import App from "petal-ui/components/app"
-import PuButton from "petal-ui/components/button"
-import PuCell from "petal-ui/components/cell"
-import PuCellGroup from "petal-ui/components/cell-group"
+// import PuiButton from "petal-ui/components/button"
+import Cell from "petal-ui/components/cell"
+import CellGroup from "petal-ui/components/cell-group"
+import Navbar from "petal-ui/components/navbar"
+// import Tabbar from "petal-ui/components/tabbar"
+import TabbarItem from "petal-ui/components/tabbar-item"
 import {usePetalUiStore} from "petal-ui/stores/petal-ui";
 
 const puiStore = usePetalUiStore()
@@ -46,7 +79,7 @@ const setTheme = () => {
 
 <style scoped>
 .content {
-    padding: 20rpx;
+    padding: 0 20rpx;
 }
 
 .content :deep(.petal-btn) {
