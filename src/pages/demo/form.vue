9c32bb0cc7128c8a4959a1f070b44c78fe8057f4
kyour-cn <kyour@outlook.com>
2023-11-06 22:57:14 +0800

    perf: 表单演示新增开关组件

--- src/pages/demo/form.vue
+++ src/pages/demo/form.vue
@@ -1,6 +1,16 @@
 <template>
     <App>
-        <PuiNavbar title="PetalUI 表单" left-arrow @click-left="goBack"/>
+        <PuiNavbar
+            title="PetalUI 表单"
+            left-arrow
+            right-icon="petal-icon-help"
+            @click-left="goBack"
+        />
+
+        <Switch v-model="switchData" />
+
+        <Radio></Radio>
+
     </App>
 </template>
 
@@ -8,10 +18,14 @@
 
 import PuiNavbar from "petal-ui/components/navbar/index.vue";
 import App from "petal-ui/components/app"
+import Switch from "petal-ui/components/switch"
+import {ref} from "vue";
 
 const goBack = () => {
     uni.navigateBack()
 }
+
+const switchData = ref(false)
 
 </script>
 
