85988fee333353287ce841d4754c5f1b514e8dec
Kyour.cn <kyour@outlook.com>
2023-11-06 17:20:23 +0800

    feat: 演示页面调整

---
+++ src/pages/demo/form.vue
@@ -0,0 +1,20 @@
+<template>
+    <App>
+        <PuiNavbar title="PetalUI 表单" left-arrow @click-left="goBack"/>
+    </App>
+</template>
+
+<script setup>
+
+import PuiNavbar from "petal-ui/components/navbar/index.vue";
+import App from "petal-ui/components/app"
+
+const goBack = () => {
+    uni.navigateBack()
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
     <App>
-        <Navbar title="PetalUI" />
-        
+        <PuiNavbar title="PetalUI"/>
+
         <view class="content">
-            <pui-button size="mini" background="green" text="开始体验" />
+            <pui-button size="mini" background="green" text="开始体验"/>
             <pui-button size="small" disabled background="error">开始体验</pui-button>
             <pui-button :block="true" size="normal">开始体验</pui-button>
             <pui-button :block="true" size="large" @click="setTheme" background="#5555FF">切换主题</pui-button>
@@ -18,6 +18,19 @@
                 <Cell title="名称" value="PetalUI"/>
                 <Cell title="框架" value="Vue3"/>
                 <Cell title="属性" value="Vite"/>
+                <Cell title="表单" value="查看演示" is-link @click="goPage('/pages/demo/form')"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
+                <Cell title="属性" value="Vite"/>
                 <Cell title="属性" value="Vite"/>
                 <Cell title="属性" value="Vite"/>
                 <Cell title="属性" value="Vite"/>
@@ -31,9 +44,9 @@
             </CellGroup>
         </view>
         <pui-tabbar>
-            <pui-tabbar-item icon="petal-icon-home" name="首页" />
-            <pui-tabbar-item icon="petal-icon-discover" activeIcon="petal-icon-discover" name="发现" />
-            <pui-tabbar-item icon="petal-icon-contacts" name="我的" />
+            <pui-tabbar-item icon="petal-icon-home" name="首页"/>
+            <pui-tabbar-item icon="petal-icon-discover" name="发现"/>
+            <pui-tabbar-item icon="petal-icon-contacts" name="我的"/>
         </pui-tabbar>
     </App>
 
@@ -45,7 +58,7 @@ import App from "petal-ui/components/app"
 import PuiButton from "petal-ui/components/button"
 import Cell from "petal-ui/components/cell"
 import CellGroup from "petal-ui/components/cell-group"
-import Navbar from "petal-ui/components/navbar"
+import PuiNavbar from "petal-ui/components/navbar"
 import PuiTabbar from "petal-ui/components/tabbar"
 import PuiTabbarItem from "petal-ui/components/tabbar-item"
 import {usePetalUiStore} from "petal-ui/stores/petal-ui";
@@ -58,8 +71,16 @@ const setTheme = () => {
 
     // puiStore.setTheme({
     //     'primary': 'red'
-    // })\
+    // })
 
+}
+
+const goPage = (page) => {
+    console.log("goto:"+page)
+    //在起始页面跳转到test.vue页面并传递参数
+    uni.navigateTo({
+        url: page
+    });
 }
 
 </script>
