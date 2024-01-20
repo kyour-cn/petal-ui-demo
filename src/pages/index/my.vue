c30bee177eddb2630d3ab1889bb36df94bc30406
Kyour.cn <kyour@outlook.com>
2024-01-20 14:13:18 +0800

    feat: 新增我的页面

--- src/pages/index/my.vue
+++ src/pages/index/my.vue
@@ -1,11 +1,18 @@
 <script setup>
+import App from "petal-ui/components/app"
+import PuiNavbar from "petal-ui/components/navbar/index.vue";
+import Tabbar from "../../components/tabbar.vue";
 
-import App from "../../../../petal-ui/components/app/index.vue";
 </script>
 
 <template>
     <App>
+        <PuiNavbar
+            title="我的"
+        />
+        none
 
+        <Tabbar :index="3"/>
     </App>
 </template>
 
