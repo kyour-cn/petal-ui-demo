1edf398b26532084e72d390b3e205396340ddcf1
Kyour.cn <kyour@outlook.com>
2024-02-02 00:52:33 +0800

    perf: 优化部分演示页面

--- src/pages/demo/form.vue
+++ src/pages/demo/form.vue
@@ -4,9 +4,11 @@ import PuiNavbar from "petal-ui/components/navbar";
 import App from "petal-ui/components/app"
 import Switch from "petal-ui/components/switch"
 import PuiSlider from "petal-ui/components/slider"
+import PuiPopover from "petal-ui/components/popover"
 import PuiCard from "petal-ui/components/card"
 import FormItem from "petal-ui/components/form-item"
 import Form from "petal-ui/components/form"
+
 import {goBack} from "../../utils"
 
 import {ref} from "vue";
@@ -35,7 +37,16 @@ const textVal = ref("")
                 </template>
             </FormItem>
             <FormItem v-model="textVal" title="文本输入" required type="text"/>
-            <FormItem title="内容显示" v-model="textVal"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
+            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
             <FormItem title="滑动条" :label="'这是一个滑动条，值为：'+ sliderValue">
                 <template #value>
                     <PuiSlider style="flex: 1" :min="10" v-model="sliderValue" />
@@ -47,6 +58,29 @@ const textVal = ref("")
             <PuiSlider v-model="sliderValue2" :min="100" :max="500"/>
             {{sliderValue2}}
         </PuiCard>
+
+        <PuiPopover direction="right">
+            <button>点击显示弹出框</button> <!-- 默认插槽 -->
+            <template #reference>
+                <view style="width: 160rpx">这是弹出内容</view>
+            </template>
+        </PuiPopover>
+
+        <PuiPopover direction="left">
+            <button>点击显示弹出框</button> <!-- 默认插槽 -->
+            <template #reference>
+                <view style="width: 160rpx">这是弹出内容</view>
+            </template>
+        </PuiPopover>
+
+
+        <PuiPopover direction="top">
+            <button>点击显示弹出框</button> <!-- 默认插槽 -->
+            <template #reference>
+                <view>这是弹出内容</view> <!-- reference插槽 -->
+            </template>
+        </PuiPopover>
+
 
     </App>
 </template>
