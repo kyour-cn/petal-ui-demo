d219e7cdf4fff3068a6bc9d4ff865dd653ff9246
Kyour.cn <kyour@outlook.com>
2024-02-03 00:30:35 +0800

    perf: 更新表单演示页面

--- src/pages/demo/form.vue
+++ src/pages/demo/form.vue
@@ -4,10 +4,11 @@ import PuiNavbar from "petal-ui/components/navbar";
 import App from "petal-ui/components/app"
 import Switch from "petal-ui/components/switch"
 import PuiSlider from "petal-ui/components/slider"
-import PuiPopover from "petal-ui/components/popover"
 import PuiCard from "petal-ui/components/card"
 import FormItem from "petal-ui/components/form-item"
 import Form from "petal-ui/components/form"
+import PuiCheckbox from "petal-ui/components/checkbox"
+import PuiRadio from "petal-ui/components/radio"
 
 import {goBack} from "../../utils"
 
@@ -38,18 +39,19 @@ const textVal = ref("")
             </FormItem>
             <FormItem v-model="textVal" title="文本输入" required type="text"/>
             <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
-            <FormItem title="内容显示" :model-value="textVal ? textVal : '无'"/>
             <FormItem title="滑动条" :label="'这是一个滑动条，值为：'+ sliderValue">
                 <template #value>
                     <PuiSlider style="flex: 1" :min="10" v-model="sliderValue" />
+                </template>
+            </FormItem>
+            <FormItem title="多选" required>
+                <template #value>
+                    <PuiCheckbox v-model="switchValue" />
+                </template>
+            </FormItem>
+            <FormItem title="单选" required>
+                <template #value>
+                    <PuiRadio v-model="switchValue" />
                 </template>
             </FormItem>
         </Form>
@@ -58,29 +60,8 @@ const textVal = ref("")
             <PuiSlider v-model="sliderValue2" :min="100" :max="500"/>
             {{sliderValue2}}
         </PuiCard>
-
-        <PuiPopover direction="right">
-            <button>点击显示弹出框</button> <!-- 默认插槽 -->
-            <template #reference>
-                <view style="width: 160rpx">这是弹出内容</view>
-            </template>
-        </PuiPopover>
-
-        <PuiPopover direction="left">
-            <button>点击显示弹出框</button> <!-- 默认插槽 -->
-            <template #reference>
-                <view style="width: 160rpx">这是弹出内容</view>
-            </template>
-        </PuiPopover>
-
-
-        <PuiPopover direction="top">
-            <button>点击显示弹出框</button> <!-- 默认插槽 -->
-            <template #reference>
-                <view>这是弹出内容</view> <!-- reference插槽 -->
-            </template>
-        </PuiPopover>
-
+        <PuiCheckbox v-model="switchValue" disabled/>
+        <PuiRadio v-model="switchValue" disabled/>
 
     </App>
 </template>
